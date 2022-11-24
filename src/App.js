3ca96c42923c3c6e01509dd08cfe6c171b679174
Abdul Muqeet Arshad <abdulmuqeet.arshad@devsinc.com>
2022-11-24 08:28:19 -0800

    wip

--- src/App.js
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className='App'>
-      <h1>Phone Book App</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+const Home = () => (
+  <div>
+    <h1>Phone Book App</h1>
+  </div>
+)
+
+export default Home
